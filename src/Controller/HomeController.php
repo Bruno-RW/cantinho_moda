245052095248dee9a733ecdde2ba55238d3ecbf2
Bruno-RW <bruno_wunsch@hotmail.com>
2023-04-12 14:41:03 -0300

    Adição Home Controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CantinhoModa\Controller;
+
+use CantinhoModa\Core\FrontController;
+use CantinhoModa\Model\Produto;
+use CantinhoModa\View\Render;
+
+class HomeController extends FrontController
+{
+    public function index()
+    {
+        $dados = [];
+        $dados['titulo'] = 'Página inicial';
+        $dados['topo'] = $this->carregaHTMLTopo();
+        $dados['rodape'] = $this->carregaHTMLRodape();
+        
+        $produto = new Produto();
+        $rowsProdutos = $produto->find(['idmarca = ' => 1]);
+
+        foreach ($rowsProdutos as &$p) {
+            $produto->loadById($p['idproduto']);
+
+            // $p['imagens'] = $produto->getFiles();
+        }
+        
+        $dados['produtos'] = $rowsProdutos;
+        
+        Render::front('home', $dados);
+    }
+}
