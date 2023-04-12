cf087e26fb0b64ac84a2e8a7886f0da4aca52331
Bruno-RW <bruno_wunsch@hotmail.com>
2023-04-12 14:42:17 -0300

    Adição do Front Controller

---
+++ src/Core/FrontController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace CantinhoModa\Core;
+
+// use CantinhoModa\Model\Categoria;
+use CantinhoModa\Model\Empresa;
+use CantinhoModa\View\Render;
+
+abstract class FrontController
+{
+    /**
+     * Alimenta com dados e renderiza o Topo do site para o cliente (front-end)
+     *
+     * @return void
+     */
+    public function carregaHTMLTopo()
+    {
+        $empresa = new Empresa();
+        $dados = $empresa->find(['tipo ='=>'Matriz']);
+
+        if ( !empty($_SESSION['cliente']) ) {
+            $dados[0]['cliente'] = $_SESSION['cliente'];
+        }
+
+        // $dados[0]['categorias'] = (new Categoria)->find();
+
+        return Render::block('topo', $dados[0]);
+    }
+
+    /**
+     * Alimenta com dados e renderiza o Rodapé do site para o cliente (front-end)
+     *
+     * @return void
+     */
+    public function carregaHTMLRodape()
+    {
+        $empresa = new Empresa();
+        $dados = $empresa->find(['tipo ='=>'Matriz']);
+
+        return Render::block('rodape', $dados[0]);
+    }
+}
