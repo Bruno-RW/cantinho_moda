1a14554680cb55f0c28d88bafeb2e524fde1d57e
Bruno-RW <bruno_wunsch@hotmail.com>
2023-04-12 14:42:51 -0300

    Criação do rodapé

---
+++ templates/blocks/rodape.php
@@ -0,0 +1,57 @@
+<section id="jornal" class="section-p1 section-m1 d-flex flex-wrap align-items-center justify-content-between">
+    <div class="info">
+        <h4>Inscreva-se para receber notícias</h4>
+        <p>Receba atualizações no e-mail sobre nossa loja e <span>ofertas especiais</span></p>
+    </div>
+    <div class="form d-flex">
+        <input type="text" placeholder="Seu endereço de e-mail">
+        <button class="normal">Inscreva-se</button>
+    </div>
+</section>
+
+<section id="footer" class="section-p1 d-flex flex-wrap justify-content-between">
+    <div class="col-4 top d-flex flex-column align-items-start">
+        <img src="img/logo-black-cortado.png" class="logo" alt="">
+        <h4>Contato</h4>
+        <p><strong>Endereço: </strong> Avenida Tucunduva, 300 - Sala 101</p>
+        <p><strong>Telefone: </strong> (55) 9 9988-0103</p>
+        <p><strong>Horários: </strong> 07:30-11:30 & 13:00-17:00, Segunda á Sexta</p>
+    </div>
+
+    <div class="col d-flex flex-column align-items-start">
+        <h4>Sobre</h4>
+        <a href="#">Sobre Nós</a>
+        <a href="#">Informações extras</a>
+        <a href="#">Política de Privacidade</a>
+        <a href="#">Termos & Condições</a>
+        <a href="#">Nosso Contato</a>
+    </div>
+
+    <div class="col d-flex flex-column align-items-start">
+        <h4>Minha Conta</h4>
+        <a href="#">Inscrever-se</a>
+        <a href="#">Favoritos</a>
+        <a href="#">Ajuda</a>
+    </div>
+
+    <div class="col pad d-flex flex-column align-items-start">
+        <h4>Métodos de Pagamento</h4>
+        <div class="pagamentos">
+            <img src="img/visa.png" alt="">
+            <img src="img/mastercard.png" alt="">
+            <img src="img/elo.png" alt="">
+            <img src="img/pix.png" alt="">
+        </div>
+        <div class="siga">
+            <h4>Nossas Redes</h4>
+            <div class="icone">
+                <i class="bi bi-facebook"></i>
+                <i class="bi bi-instagram"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright w-100 ">
+        <p>© 2023 Cantinho da Moda. Todos os direitos reservados.</p>
+    </div>
+</section>
