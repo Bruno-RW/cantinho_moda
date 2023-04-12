6a18b75d48cbd194ba29754f4f84b17007363028
Bruno-RW <bruno_wunsch@hotmail.com>
2023-04-12 14:43:00 -0300

    Criação do topo

---
+++ templates/blocks/topo.php
@@ -0,0 +1,19 @@
+<section id="header" class="d-flex align-items-center justify-content-between position-sticky">
+        <a href="#"><img src="img/logo-black.png" class="logo" alt=""></a>
+
+        <div>
+            <ul id="navbar">
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">Loja</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+                <li id="li-usuario"><a href="pages/login.html"><i class="fa fa-regular fa-user"></i></a></li>
+                <a href="#" id="fechar"><i class="fa-solid fa-xmark"></i></a>
+            </ul>
+        </div>
+
+        <div id="celular">
+            <a href="#"><i class="fa fa-regular fa-user"></i></a>
+            <i id="bar" class="fa fa-solid fa-bars"></i>
+        </div>
+</section>
