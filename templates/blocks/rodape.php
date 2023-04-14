d9e19636b4c8805b4d92571546d1d06c6585b76a
Bruno-RW <bruno_wunsch@hotmail.com>
2023-04-14 13:28:41 -0300

    Pequenas alterações de design

--- templates/blocks/rodape.php
+++ templates/blocks/rodape.php
@@ -11,20 +11,20 @@
 
 <section id="footer" class="section-p1 d-flex flex-wrap justify-content-between">
     <div class="col-4 top d-flex flex-column align-items-start">
-        <img src="img/logo-black-cortado.png" class="logo" alt="">
+        <img src="/assets/img/icones/logos/logo-black-cortado.png" class="logo" alt="">
         <h4>Contato</h4>
+        <p><strong>Horários: </strong> 07:30-11:30 & 13:00-17:00, Segunda á Sexta</p>
         <p><strong>Endereço: </strong> Avenida Tucunduva, 300 - Sala 101</p>
         <p><strong>Telefone: </strong> (55) 9 9988-0103</p>
-        <p><strong>Horários: </strong> 07:30-11:30 & 13:00-17:00, Segunda á Sexta</p>
     </div>
 
     <div class="col d-flex flex-column align-items-start">
         <h4>Sobre</h4>
-        <a href="#">Sobre Nós</a>
-        <a href="#">Informações extras</a>
         <a href="#">Política de Privacidade</a>
         <a href="#">Termos & Condições</a>
+        <a href="#">Informações extras</a>
         <a href="#">Nosso Contato</a>
+        <a href="#">Sobre Nós</a>
     </div>
 
     <div class="col d-flex flex-column align-items-start">
@@ -37,16 +37,23 @@
     <div class="col pad d-flex flex-column align-items-start">
         <h4>Métodos de Pagamento</h4>
         <div class="pagamentos">
-            <img src="img/visa.png" alt="">
-            <img src="img/mastercard.png" alt="">
-            <img src="img/elo.png" alt="">
-            <img src="img/pix.png" alt="">
+            <img src="/assets/img/icones/pagamentos/visa.png" alt="">
+            <img src="/assets/img/icones/pagamentos/mastercard.png" alt="">
+            <img src="/assets/img/icones/pagamentos/elo.png" alt="">
+            <img src="/assets/img/icones/pagamentos/pix.png" alt="">
         </div>
         <div class="siga">
             <h4>Nossas Redes</h4>
             <div class="icone">
-                <i class="bi bi-facebook"></i>
-                <i class="bi bi-instagram"></i>
+                <a href="https://www.facebook.com/Cantinho-da-Moda-HZ-108953705391360" target="_blank">
+                    <i class="fa-brands fa-facebook"></i>
+                </a>
+                <a href="https://www.instagram.com/cantinhodamodahz/" target="_blank">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="https://api.whatsapp.com/send?phone=555599880103&data=AWBHrFuxGJ0C9sFUac-Lu26ROGuk_XJ7_ppVORczJdORXvysLB0Bi9MFZAwpodxI8bB6r-87wOL_Gd8EBfJb9elrwXQ71AwAPTV_9sUxAaQzCXSKm9XkMVXBwzcKfSAzxQQjMwsneS_TBujYs8y7lP88W9LdwDK9GrQy6QAM-g7KOYa1U6kol0z9ExGRZO9qic-nd2X-QVKBzK11qE1-jaxXQmtTcvwFtWoTWcq9tw8Wpv2ZJcScebRltzB45cnIq3gRJi-P_01NuVaB4StJ3eBMnQVEwuP86AvhlcdpfnpPHomIiaE&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR38Q4fCiTPhXLXkHKGgECGv58MCi82LvXmBKqiyjXRPL9Vf3_Qon-7KjgY">
+                    <i class="fa-brands fa-whatsapp"></i>
+                </a>
             </div>
         </div>
     </div>
