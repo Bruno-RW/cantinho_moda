05ef464658a9b5bd729fb628c75788d7f96b8d46
Bruno-RW <bruno_wunsch@hotmail.com>
2023-04-17 16:28:42 -0300

    Criação de página 404

---
+++ templates/frontend/pages/404.php
@@ -0,0 +1,16 @@
+<?php
+    $link = URL . $_SERVER['REQUEST_URI'];
+?>
+
+<section id="erro" class="section-p1 d-flex align-items-center justify-content-center">
+    <div class="container d-flex flex-column align-items-center justify-content-center">
+        <h1>404</h1>
+        <h4>Ops! Página não encontrada</h4>
+        <p>Desculpe, a página <span><?=$link?></span> não existe. Caso tenha algo de errado, entre em contado.</p>
+
+        <div class="btn">
+            <a href="/">Voltar a home</a>
+            <a href="/contato">Reportar problema</a>
+        </div>
+    </div>
+</section>
