05d13971f7a05f861234f07b2cdc9de953acecd8
Bruno-RW <bruno_wunsch@hotmail.com>
2023-04-19 16:37:08 -0300

    Criação de classe de Favorito

---
+++ src/Model/Favorito.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace CantinhoModa\Model;
+
+use CantinhoModa\Core\Attribute\Campo;
+use CantinhoModa\Core\Attribute\Entidade;
+use CantinhoModa\Core\DAO;
+
+#[Entidade(name: 'favoritos')]
+class Favorito extends DAO
+{
+    #[Campo(label: 'Cód. Favorito', pk: true, nn: true, auto: true)]
+    protected $idFavorito;
+
+    #[Campo(label: 'Cód. Produto', nn: true)]
+    protected $idProduto;
+
+    #[Campo(label: 'Cód. Cliente', nn: true)]
+    protected $idCliente;
+
+    #[Campo(label: 'Ativo', nn: true)]
+    protected $ativo;
+
+    #[Campo(label: 'Dt. Criação', nn: true, auto: true)]
+    protected $created_at;
+
+    #[Campo(label: 'Dt. Alteração', nn: true, auto: true)]
+    protected $updated_at;
+
+    public function getIdFavorito()
+    {
+        return $this->idFavorito;
+    }
+
+    public function getIdProduto()
+    {
+        return $this->idProduto;
+    }
+
+    public function setIdProduto($idProduto): self
+    {
+        $this->idProduto = $idProduto;
+
+        return $this;
+    }
+
+    public function getIdCliente()
+    {
+        return $this->idCliente;
+    }
+
+    public function setIdCliente($idCliente): self
+    {
+        $this->idCliente = $idCliente;
+
+        return $this;
+    }
+
+    public function getAtivo()
+    {
+        return $this->ativo;
+    }
+
+    public function setAtivo($ativo): self
+    {
+        $this->ativo = $ativo;
+
+        return $this;
+    }
+
+    public function getCreated_At()
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdated_At()
+    {
+        return $this->updated_at;
+    }
+}
