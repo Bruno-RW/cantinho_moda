4fa93cde5d2897656fca126c9cece6583f412574
Bruno-RW <bruno_wunsch@hotmail.com>
2023-04-21 19:43:40 -0300

    Adição de responsividade da pág Admin Login

--- templates/frontend/pages/admin-login.php
+++ templates/frontend/pages/admin-login.php
@@ -1,6 +1,6 @@
 <section id="login-admin" class="section-p1 d-flex align-items-center justify-content-center">
     <div class="container">
-        <div class="titulo">
+        <div class="titulo d-flex flex-column align-items-center justify-content-center">
             <h1>ADMIN LOGIN</h1>
         </div>
         <?= retornaHTMLAlertMensagemSessao() ?>
