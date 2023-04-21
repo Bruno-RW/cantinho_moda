0b696e3a1a793b20a9143c2aba372da0709fd44c
Bruno-RW <bruno_wunsch@hotmail.com>
2023-04-21 17:37:41 -0300

    Alterações gerais

--- src/Core/App.php
+++ src/Core/App.php
@@ -49,10 +49,8 @@ class App
     {
         self::$router->get('/','\CantinhoModa\Controller\HomeController@index');
 
-        // self::$router->post('/ajax','\CantinhoModa\Controller\AjaxController@loader');
+        // self::$router->get('/loja','\CantinhoModa\Controller\LojaController@loja');
 
-        // self::$router->get('/busca','\CantinhoModa\Controller\BuscaController@buscar');
-        
         self::$router->get('/sobre','\CantinhoModa\Controller\SobreController@sobre');
 
         self::$router->get('/contato','\CantinhoModa\Controller\ContatoController@contato');
@@ -65,10 +63,13 @@ class App
         self::$router->get('/cadastro','\CantinhoModa\Controller\CadastroController@cadastro');
         self::$router->post('/cadastro','\CantinhoModa\Controller\CadastroController@postCadastro');
 
+        // self::$router->get('/favoritos','\CantinhoModa\Controller\FavoritosController@favoritos');
         // self::$router->get('/meus-dados','\CantinhoModa\Controller\MeusDadosController@meusDados');
+
         // self::$router->get('/produtos/{id}','\CantinhoModa\Controller\ProdutoController@listar');
-        // self::$router->get('/sobre-nos','\CantinhoModa\Controller\SobreNosController@listar');
-        // self::$router->get('/favoritos','\CantinhoModa\Controller\FavoritosController@listar');
+
+        self::$router->post('/ajax','\CantinhoModa\Controller\AjaxController@loader');
+        self::$router->get('/busca','\CantinhoModa\Controller\BuscaController@buscar');
     }
 
     /**
@@ -77,47 +78,47 @@ class App
      *
      * @return void
      */
-    // private static function registraRotasDoBackend()
-    // {
-    //     self::$router->before('GET|POST', '/admin/.*', function (){
-    //         if ( empty($_SESSION['usuario']) ) {
-    //             redireciona('/admin', 'danger', 'Faça seu logon para continuar');
-    //         }
-    //     });
+    private static function registraRotasDoBackend()
+    {
+        self::$router->before('GET|POST', '/admin/.*', function (){
+            if ( empty($_SESSION['usuario']) ) {
+                redireciona('/admin', 'danger', 'Faça seu logon para continuar');
+            }
+        });
 
-    //     self::$router->mount('/admin', function() {
-    //         self::$router->get ('/','\CantinhoModa\Controller\AdminLoginController@login');
-    //         self::$router->post('/','\CantinhoModa\Controller\AdminLoginController@postLogin');
+        self::$router->mount('/admin', function() {
+            self::$router->get ('/','\CantinhoModa\Controller\AdminLoginController@login');
+            self::$router->post('/','\CantinhoModa\Controller\AdminLoginController@postLogin');
 
-    //         self::$router->get('/dashboard','\CantinhoModa\Controller\AdminDashboardController@index');
+            self::$router->get('/dashboard','\CantinhoModa\Controller\AdminDashboardController@index');
 
-    //         self::$router->get('/remover/(\w+)/(\d+)','\CantinhoModa\Controller\AdminRemoveController@acao');
+            self::$router->get('/remover/(\w+)/(\d+)','\CantinhoModa\Controller\AdminRemoveController@acao');
 
-    //         self::$router->get ('/clientes',        '\CantinhoModa\Controller\AdminClienteController@listar');
-    //         self::$router->get ('/clientes/{valor}','\CantinhoModa\Controller\AdminClienteController@form');
-    //         self::$router->post('/clientes/{valor}','\CantinhoModa\Controller\AdminClienteController@postForm');
+            self::$router->get ('/clientes',        '\CantinhoModa\Controller\AdminClienteController@listar');
+            self::$router->get ('/clientes/{valor}','\CantinhoModa\Controller\AdminClienteController@form');
+            self::$router->post('/clientes/{valor}','\CantinhoModa\Controller\AdminClienteController@postForm');
 
-    //         self::$router->get ('/usuarios',        '\CantinhoModa\Controller\AdminUsuarioController@listar');
-    //         self::$router->get ('/usuarios/{valor}','\CantinhoModa\Controller\AdminUsuarioController@form');
-    //         self::$router->post('/usuarios/{valor}','\CantinhoModa\Controller\AdminUsuarioController@postForm');
+            self::$router->get ('/usuarios',        '\CantinhoModa\Controller\AdminUsuarioController@listar');
+            self::$router->get ('/usuarios/{valor}','\CantinhoModa\Controller\AdminUsuarioController@form');
+            self::$router->post('/usuarios/{valor}','\CantinhoModa\Controller\AdminUsuarioController@postForm');
 
-    //         self::$router->get ('/categorias',        '\CantinhoModa\Controller\AdminCategoriaController@listar');
-    //         self::$router->get ('/categorias/{valor}','\CantinhoModa\Controller\AdminCategoriaController@form');
-    //         self::$router->post('/categorias/{valor}','\CantinhoModa\Controller\AdminCategoriaController@postForm');
+            self::$router->get ('/categorias',        '\CantinhoModa\Controller\AdminCategoriaController@listar');
+            self::$router->get ('/categorias/{valor}','\CantinhoModa\Controller\AdminCategoriaController@form');
+            self::$router->post('/categorias/{valor}','\CantinhoModa\Controller\AdminCategoriaController@postForm');
 
-    //         self::$router->get ('/marcas',        '\CantinhoModa\Controller\AdminMarcaController@listar');
-    //         self::$router->get ('/marcas/{valor}','\CantinhoModa\Controller\AdminMarcaController@form');
-    //         self::$router->post('/marcas/{valor}','\CantinhoModa\Controller\AdminMarcaController@postForm');
+            self::$router->get ('/marcas',        '\CantinhoModa\Controller\AdminMarcaController@listar');
+            self::$router->get ('/marcas/{valor}','\CantinhoModa\Controller\AdminMarcaController@form');
+            self::$router->post('/marcas/{valor}','\CantinhoModa\Controller\AdminMarcaController@postForm');
             
-    //         self::$router->get ('/produtos',        '\CantinhoModa\Controller\AdminProdutoController@listar');
-    //         self::$router->get ('/produtos/{valor}','\CantinhoModa\Controller\AdminProdutoController@form');
-    //         self::$router->post('/produtos/{valor}','\CantinhoModa\Controller\AdminProdutoController@postForm');
+            self::$router->get ('/produtos',        '\CantinhoModa\Controller\AdminProdutoController@listar');
+            self::$router->get ('/produtos/{valor}','\CantinhoModa\Controller\AdminProdutoController@form');
+            self::$router->post('/produtos/{valor}','\CantinhoModa\Controller\AdminProdutoController@postForm');
            
-    //         self::$router->get ('/imagens/(\w+)/(\d+)',      '\CantinhoModa\Controller\AdminImagemController@listar');
-    //         self::$router->get ('/imagens/(\w+)/(\d+)/(\w+)','\CantinhoModa\Controller\AdminImagemController@form');
-    //         self::$router->post('/imagens/(\w+)/(\d+)/(\w+)','\CantinhoModa\Controller\AdminImagemController@postForm');
-    //     });
-    // }
+            self::$router->get ('/imagens/(\w+)/(\d+)',      '\CantinhoModa\Controller\AdminImagemController@listar');
+            self::$router->get ('/imagens/(\w+)/(\d+)/(\w+)','\CantinhoModa\Controller\AdminImagemController@form');
+            self::$router->post('/imagens/(\w+)/(\d+)/(\w+)','\CantinhoModa\Controller\AdminImagemController@postForm');
+        });
+    }
 
     /**
      * Registra rota genérica para erro de URL digitada
