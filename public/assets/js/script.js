1e177b9bd2a175b3fb746da39936a21bd7dc81f2
Bruno-RW <bruno_wunsch@hotmail.com>
2023-04-21 16:17:16 -0300

    Adição de função de favoritar e ajax

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,15 +1,109 @@
-const bar = document.getElementById('bar');
-const fecharBar = document.getElementById('fechar');
-const navbar = document.getElementById('navbar');
+const bar = document.querySelector('#header #bar');
+const fecharBar = document.querySelector('#header #fechar');
+const navbar = document.querySelector('#header #navbar');
+// const secao = document.querySelector('section :not(#header)');
 
-if (bar) {
-    bar.addEventListener('click', () => {
-        navbar.classList.add('active');
-    })
-}
+bar.addEventListener('click', () => {
+    navbar.classList.add('ativo');
+});
 
-if (fecharBar) {
-    fecharBar.addEventListener('click', () => {
-        navbar.classList.remove('active');
-    })
+fecharBar.addEventListener('click', () => {
+    navbar.classList.remove('ativo');
+});
+
+// secao.addEventListener('click', () => {
+//     navbar.classList.remove('ativo');
+// });
+
+const container = document.querySelector('#login .container');
+const linkLogar = document.querySelector('#login .link-logar');
+const linkRegistrar = document.querySelector('#login .link-registar');
+
+linkRegistrar.addEventListener('click', () => {
+    container.classList.add('ativo');
+});
+
+linkLogar.addEventListener('click', () => {
+    container.classList.remove('ativo');
+});
+
+// ADICIONA COMPORTAMENTO DE CURTIR AO BOTÃO DE CURTIR / FAVORITAR
+document.querySelectorAll('.favoritar').forEach(linkCurtir => {
+    linkCurtir.addEventListener('click', e => {
+        e.preventDefault();
+        let dadosPost = new FormData();
+        dadosPost.append('acao', 'curtir');
+        dadosPost.append('idproduto', linkCurtir.dataset.idproduto);
+        ajax('/ajax', dadosPost, function(resposta) {
+            if (resposta.status != 'success') {
+                Swal.fire({
+                    icon: resposta.status,
+                    title: 'Opsss...',
+                    text: resposta.mensagem
+                });
+                return;
+            }
+            // Se deu tudo certo, executa o código abaixo
+            if (resposta.dados.curtiu) {
+                linkCurtir.querySelector('i').classList.remove('regular');
+                linkCurtir.querySelector('i').classList.add('solid');
+            } else {
+                linkCurtir.querySelector('i').classList.remove('solid');
+                linkCurtir.querySelector('i').classList.add('regular');
+            }
+        });
+    });
+});
+
+function ajax(url, dados, callBack) {
+    if (!url, !dados, !callBack) {
+        throw 'Todos os parâmetros devem ser preenchidos';
+    }
+
+    let dadosCallBack = {};
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    xhr.onload = function() {
+        if (xhr.readyState == 4) {
+            if (xhr.status != 200) {
+                Swal.fire({
+                    position: 'top-center',
+                    icon: 'warning',
+                    title: 'Falha na comunicação',
+                    text: 'Ocorreu um erro de conexão, por favor, tente novamente. Se o erro persistir, contate o suporte',
+                    showConfirmButton: false,
+                    timer: 3000
+                });
+                return;
+            }
+            try {
+                dadosCallBack = JSON.parse( xhr.responseText );
+            } catch(e) {
+                Swal.fire({
+                    position: 'top-center',
+                    icon: 'warning',
+                    title: 'Falha de processamento',
+                    text: 'A resposta não pôde ser processada, tente novamente ou entre em contato com o suporte',
+                    showConfirmButton: false,
+                    timer: 3000
+                });
+                return;
+            }
+
+            callBack(dadosCallBack);
+        }
+    };
+
+    xhr.onerror = function() {
+        Swal.fire({
+            position: 'top-end',
+            icon: 'error',
+            title: 'Falha na comunicação',
+            text: 'Ocorreu um erro de conexão, por favor, tente novamente',
+            showConfirmButton: false,
+            timer: 3000
+        });
+    };
+
+    xhr.send(dados);
 }
