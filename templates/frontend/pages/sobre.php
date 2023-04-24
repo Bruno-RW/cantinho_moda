3f94a2448718d811fdd92fa0e3b27a0dfa7efd77
Bruno-RW <bruno_wunsch@hotmail.com>
2023-04-24 15:56:44 -0300

    Criação de página Sobre

---
+++ templates/frontend/pages/sobre.php
@@ -0,0 +1,44 @@
+<section id="header-banner" class="sobre-banner">
+    <h2>#SobreNós</h2>
+    <p>Descubra mais sobre nós e nossa loja</p>
+</section>
+
+<section id="detalhes-sobre" class="section-p1 d-flex align-center">
+    <img src="/assets/img/banners/b9.png" alt="">
+    <div class="d-flex flex-column justify-content-between">
+        <h1>Quem somos?</h1>
+        <p>A empresa Cantinho da Moda é uma loja de moda focada em vender roupas e acessórios para mulheres de diversos gostos e necessidades. A Cantinho da Moda está sempre atualizada com as últimas tendências da moda, garantindo que nossos clientes estejam sempre elegantes.</p>
+        <p>Além disso, nos orgulhamos de oferecer roupas e acessórios de alta qualidade. Então, se você está procurando uma loja de moda confiável e de alta qualidade, não procure mais do que a Cantinho da Moda. Venha nos visitar e veja por si mesmo tudo o que temos a oferecer!</p>
+    </div>
+</section>
+
+<section id="perguntas-frequentes" class="section-p1 d-flex flex-column">
+    <h2>Perguntas frequentes</h2>
+
+    <div class="pro-container row justify-content-center">
+        <div class="pro">
+            <h3>Por que não consigo comprar produtos?</h3>
+            <p>Este site possui o objetivo de expor os itens no catálogo da loja Cantinho da Moda. Caso esteja a fim de realizar a compra de produtos, dirija-se até a nossa loja física.</p><br>
+            <p>Caso necessário, entre na página de <a href="/contato">contato</a>.</p>
+        </div>
+        
+        <div class="pro">
+            <h3>Que tipos de produtos são vendidos?</h3>
+            <p>Na Cantinho da Moda vendemos desde roupas de verão até roupas de inverno. Além disso, também vendemos alguns acessórios, como pulseiras, brincos e colares.</p><br> 
+            <p>Vá para a nossa página e aproveite: <a href="/catalogo">catálogo</a>.</p>
+        </div>
+
+        <div class="pro">
+            <h3>Como faço para utilizar a página "catálogo"?</h3>
+            <p>Para utilizar a página basta alterar as personalizações de pesquisa através de categorias predefinidas e ordem de visualização entre preços maiores e menores de produtos.</p> 
+            <p>Vá agora e teste novos produtos: <a href="/catalogo">catálogo</a>.</p>
+        </div>
+
+        <div class="pro">
+            <h3>Quais são os métodos de pagamento?</h3>
+            <p>Ao visitar nossa loja física aceitamos cartões de débito e crédito da Visa, Mastercard e Elo. Ademais, também aceitmos pagamento por Pix.</p><br><br>
+            <p>Caso tenha dúvidas, visite a página de <a href="/contato">catálogo</a>.</p>
+        </div>
+
+    </div>
+</section>
