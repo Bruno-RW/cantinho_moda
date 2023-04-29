4bcd4dac919b9543b233f838ab21b0a6244e5d95
Bruno-RW <bruno_wunsch@hotmail.com>
2023-04-29 16:10:48 -0300

    Correção na criação dos arquivos de log

--- config/parameters.php
+++ config/parameters.php
@@ -10,4 +10,4 @@ ini_set('display_errors', DISPLAY_ERRORS); // O usuário vê os erros?
 ini_set('log_erros', 1); // Devo logar os erros?
 
 // Definir o arquivo que armazenará os erros de log
-ini_set('error_log', PATH_PROJETO . 'log/error-'.date('Y').'-'.date('m').'txt');
+ini_set('error_log', PATH_PROJETO . 'log/error-'.date('Y').'-'.date('m').'.txt');
