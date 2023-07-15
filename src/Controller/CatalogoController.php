75f7f205a61986f18a1ada843626b7fc2cd1e9fa
Bruno-RW <bruno_wunsch@hotmail.com>
2023-07-15 09:03:13 -0300

    Sistema de filtros do Controller da pág catálogo

--- src/Controller/CatalogoController.php
+++ src/Controller/CatalogoController.php
@@ -17,13 +17,60 @@ class CatalogoController extends FrontController
         $dados['titulo'] = 'Catálogo';
         $dados['topo'] = $this->carregaHTMLTopo();
         $dados['rodape'] = $this->carregaHTMLRodape();
-                     
-        $sql = 'SELECT p.idproduto, p.idmarca, p.idcategoria, p.nome, p.preco, p.tamanho, f.ativo, m.marca
+        
+        switch($_GET['filtro'] ?? 'De A a Z') {
+            case 'De Z a A': 
+                $orderBy = 'ORDER BY p.nome DESC, p.preco'; 
+                break;
+            case 'Maior preço': 
+                $orderBy = 'ORDER BY p.preco DESC, p.nome'; 
+                break;
+            case 'Menor preço': 
+                $orderBy = 'ORDER BY p.preco, p.nome'; 
+                break;
+            default:
+                $orderBy = 'ORDER BY p.nome, p.preco';
+        }
+
+        $categoriasFiltradas = [];
+        foreach($_GET['categoria']??[] as $val) {
+            if ( is_numeric($val) ) {
+                $categoriasFiltradas[] = (int) $val;
+            }
+        }
+
+        $marcasFiltradas = [];
+        foreach($_GET['marca']??[] as $val) {
+            if ( is_numeric($val) ) {
+                $marcasFiltradas[] = (int) $val;
+            }
+        }
+
+        $where = '';
+        if ($categoriasFiltradas || $marcasFiltradas) {
+            if ($categoriasFiltradas) {
+                $where = 'WHERE c.idcategoria IN ('.implode(',', $categoriasFiltradas).') ';
+            }
+            if ($marcasFiltradas) {
+                if (!$where) {
+                    $where = 'WHERE c.idcategoria IN ('.implode(',', $marcasFiltradas).') ';
+                } else {
+                    $where .= 'AND m.marca IN ('.implode(',', $marcasFiltradas).') ';
+                }
+            }
+        }
+
+        //p.preco between menorpreco and maiorpreco
+        //if ($where)
+
+        $sql = "SELECT p.idproduto, p.idmarca, p.idcategoria, p.nome, p.preco, p.tamanho, f.ativo, m.marca
                 FROM produtos p
                 INNER JOIN categorias c ON c.idcategoria = p.idcategoria
                 INNER JOIN marcas m ON m.idmarca = p.idmarca
                 LEFT JOIN favoritos f ON f.idproduto = p.idproduto
-                            AND f.idcliente = ?';
+                            AND f.idcliente = ?
+                {$where}
+                {$orderBy}";
         $parametros = [ $_SESSION['cliente']['idcliente'] ?? 0];
         $rowsProdutos = DB::select($sql, $parametros);
 
