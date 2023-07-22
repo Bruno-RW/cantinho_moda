a52cf4fa33c6c482d76ef237ffee0d77c74597f7
Bruno-RW <bruno_wunsch@hotmail.com>
2023-07-22 15:39:22 -0300

    Atualização de conteúdos da pág Home

--- templates/frontend/pages/home.php
+++ templates/frontend/pages/home.php
@@ -30,11 +30,10 @@
     <div class="pro-container d-flex flex-wrap">
         <?php
             $tempTricot = 0;
-            $tempCasaco = 0;
             foreach($produtos as $p) {
                 $nome = strlen($p['nome']) <= 60 ? $p['nome']  : substr($p['nome'], 0, 57) . '...';
                 $precoTotal = number_format($p['preco'], 2, ',', '.');
-                $favoritar = ($p['ativo'] == 'S') ? 'solid' : 'regular';
+                $favorito = ($p['ativo'] == 'S') ? "style= 'font-weight: 600; color: #FF2B1C'" : "style='font-weight: 500'";
 
                 if($p['idcategoria'] == 7) {
                     if ($tempTricot < 4) {
@@ -47,31 +46,12 @@
                                     <h4>R\${$precoTotal}</h4>
                                 </div>
                                 <a href="#" class="favoritar" data-idproduto="{$p['idproduto']}" title="Favoritar produto">
-                                    <i class="fa-{$favoritar} fa-heart"></i>
+                                    <i class="fa-regular fa-heart" $favorito></i>
                                 </a>
                             </div>
                         HTML;
                     }
                     $tempTricot++;
-                }
-                
-                if($p['idcategoria'] == 3) {
-                    if ($tempCasaco < 4) {
-                        echo <<<HTML
-                            <div class="pro">
-                                <img src="{$p['imagens'][0]['url']}" alt="">
-                                <div class="des">
-                                    <span>{$p['marca']}</span>
-                                    <h5>{$nome}</h5>
-                                    <h4>R\${$precoTotal}</h4>
-                                </div>
-                                <a href="#" class="favoritar" data-idproduto="{$p['idproduto']}" title="Favoritar produto">
-                                    <i class="fa-{$favoritar} fa-heart"></i>
-                                </a>
-                            </div>
-                        HTML;
-                    }
-                    $tempCasaco++;
                 }
             }
         ?>
@@ -81,7 +61,7 @@
 <section id="banner" class="section-m1 d-flex flex-column align-items-center justify-content-center text-center">
     <h4>Produtos de qualidade </h4>
     <h2>Até <span>15% off</span> - Roupas & Acessórios</h2>
-    <button class="normal">Explore Mais</button>
+    <a href="/catalogo">Explore Mais</a>
 </section>
 
 <section id="produtos" class="section-p1 text-center">
@@ -91,10 +71,11 @@
     <div class="pro-container d-flex flex-wrap">
         <?php
             $tempShort = 0;
+            $tempSaia = 0;
             foreach($produtos as $p) {
                 $nome = strlen($p['nome']) <= 60 ? $p['nome']  : substr($p['nome'], 0, 57) . '...';
                 $precoTotal = number_format($p['preco'], 2, ',', '.');
-                $favoritar = ($p['ativo'] == 'S') ? 'solid' : 'regular';
+                $favorito = ($p['ativo'] == 'S') ? "style= 'font-weight: 600; color: #FF2B1C'" : "style='font-weight: 500'";
 
                 if($p['idcategoria'] == 6) {
                     if ($tempShort < 4) {
@@ -107,12 +88,31 @@
                                     <h4>R\${$precoTotal}</h4>
                                 </div>
                                 <a href="#" class="favoritar" data-idproduto="{$p['idproduto']}" title="Favoritar produto">
-                                    <i class="fa-{$favoritar} fa-heart"></i>
+                                    <i class="fa-regular fa-heart" $favorito></i>
                                 </a>
                             </div>
                         HTML;
                     }
                     $tempShort++;
+                }
+
+                if($p['idcategoria'] == 5) {
+                    if ($tempSaia < 4) {
+                        echo <<<HTML
+                            <div class="pro" data-idproduto="{$p['idproduto']}">
+                                <img src="{$p['imagens'][1]['url']}" alt="">
+                                <div class="des">
+                                    <span>{$p['marca']}</span>
+                                    <h5>{$nome}</h5>
+                                    <h4>R\${$precoTotal}</h4>
+                                </div>
+                                <a href="#" class="favoritar" data-idproduto="{$p['idproduto']}" title="Favoritar produto">
+                                    <i class="fa-regular fa-heart" $favorito></i>
+                                </a>
+                            </div>
+                        HTML;
+                    }
+                    $tempSaia++;
                 }
             }
         ?>
@@ -124,23 +124,23 @@
         <h4>O melhor para você</h4>
         <h2>Qualidade e elegância</h2>
         <span>Estilo é uma forma de viver</span>
-        <button class="white">Veja Mais</button>
+        <a href="/catalogo" class="white">Veja Mais</a>
     </div>
     <div class="banner-box banner-box2 d-flex flex-column align-items-start justify-content-center">
         <h4>Inverno & Verão</h4>
         <h2>Roupas diversificadas</h2>
         <span>Diversos modelos de roupa para o seu gosto</span>
-        <button class="white">Veja Mais</button>
+        <a href="/catalogo" class="white">Veja Mais</a>
     </div>
 </section>
 
 <section id="banner3" class="d-flex flex-wrap justify-content-between">
     <div class="banner-box d-flex flex-column align-items-start justify-content-center">
-        <h2>VENDA DE ACESSÓRIOS</h2>
-        <h4>Pulseiras e colares</h4>
+        <h2>DIVERSOS ACESSÓRIOS</h2>
+        <h4>Pulseiras, colares, etc</h4>
     </div>
     <div class="banner-box banner-box2 d-flex flex-column align-items-start justify-content-center">
-        <h2>NOVA COLEÇÃO DE CASACOS</h2>
+        <h2>NOVOS CASACOS</h2>
         <h4>Coleção Inverno - 2023</h4>
     </div>
     <div class="banner-box banner-box3 d-flex flex-column align-items-start justify-content-center">
