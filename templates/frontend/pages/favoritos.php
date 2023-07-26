e3987b322e0c17b1167ebbed9c06919f0c390d1f
Bruno-RW <bruno_wunsch@hotmail.com>
2023-07-26 17:44:38 -0300

    Adicionada estilização da página de Favoritos

--- templates/frontend/pages/favoritos.php
+++ templates/frontend/pages/favoritos.php
@@ -1,36 +1,51 @@
-<section id="header-banner" class="minha-conta-banner">
+<section id="modalProduto" class="modal fade" tabindex="-1" aria-labelledby="modalProdutoLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered justify-content-center">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="modalProdutoLabel"></h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body d-flex flex-column">
+
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section id="header-banner" class="favoritos-banner">
     <h2>#Favoritos</h2>
     <p>Veja seus produtos favoritos!</p>
 </section>
 
-<section id="info-conta" class="section-p1 d-flex flex-column">
-    <div class="favoritos">
-        <h2>PRODUTOS FAVORITOS</h2>
-        <div class="pro-container d-flex flex-wrap justify-content-center">
-            <?php
-                foreach($produtos as $p) {
-                    $nome = strlen($p['nome']) <= 60 ? $p['nome']  : substr($p['nome'], 0, 57) . '...';
-                    $precoTotal = number_format($p['preco'], 2, ',', '.');
-                    $favorito = ($p['ativo'] == 'S') ? "style= 'font-weight: 600;'" : "style='font-weight: 500'";
-                    $title = ($p['ativo'] == 'S') ? "title='Produto favorito'" : "title='Produto não favorito'";
+<section id="favoritos" class="section-p1 d-flex flex-column">
+    <h1>Produtos Favoritos</h1>
+    <div class="pro-container d-flex flex-wrap justify-content-start">
+        <?php
+            foreach($produtos as $p) {
+                $nome = strlen($p['nome']) <= 60 ? $p['nome']  : substr($p['nome'], 0, 57) . '...';
+                $precoTotal = number_format($p['preco'], 2, ',', '.');
+                $favorito = ($p['ativo'] == 'S') ? "style= 'font-weight: 600;'" : "style='font-weight: 500'";
+                $title = ($p['ativo'] == 'S') ? "title='Produto favorito'" : "title='Produto não favorito'";
 
-                    if($p['ativo'] == 'S') {
-                        echo <<<HTML
-                            <div class="pro" data-idproduto="{$p['idproduto']}">
-                                <img src="{$p['imagens'][0]['url']}" alt="">
-                                <div class="des">
-                                    <span>{$p['marca']}</span>
-                                    <h5>{$nome}</h5>
-                                    <h4>R\${$precoTotal}</h4>
-                                </div>
-                                <a href="#" class="favoritar" data-idproduto="{$p['idproduto']}" $title>
-                                    <i class="fa-regular fa-heart" $favorito></i>
-                                </a>
+                if($p['ativo'] == 'S') {
+                    echo <<<HTML
+                        <div class="pro" data-idproduto="{$p['idproduto']}">
+                            <img src="{$p['imagens'][0]['url']}" alt="">
+                            <div class="des">
+                                <span>{$p['marca']}</span>
+                                <h5>{$nome}</h5>
+                                <h4>R\${$precoTotal}</h4>
                             </div>
-                        HTML;
-                    }
+                            <a href="#" class="favoritar" data-idproduto="{$p['idproduto']}" $title>
+                                <i class="fa-regular fa-heart" $favorito></i>
+                            </a>
+                        </div>
+                    HTML;
                 }
-            ?>
-        </div>
+            }
+        ?>
     </div>
 </section>
