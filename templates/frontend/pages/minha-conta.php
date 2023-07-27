9c58232d24df7efd53bda7ef8e431169b7171841
Bruno-RW <bruno_wunsch@hotmail.com>
2023-07-27 17:33:07 -0300

    Add responsividade págs Favoritos e Minha Conta

--- templates/frontend/pages/minha-conta.php
+++ templates/frontend/pages/minha-conta.php
@@ -7,9 +7,15 @@
     <h1>Informações Pessoais</h1>
     <div class="conta d-flex flex-column">
         <div class="conteudo d-flex flex-column">
-            <p>Nome:   <span><?= $dados['cliente']['nome'] ?></span></p>
-            <p>E-mail: <span><?= $dados['cliente']['email'] ?></span></p>
-            <p>Dt. Cadastro: <span><?= $dados['cliente']['created_at'] ?></span></p>
+            <div class="topico d-flex">
+                <p>Nome:</p><span><?= $dados['cliente']['nome'] ?></span>
+            </div>
+            <div class="topico d-flex">
+                <p>E-mail:</p><span><?= $dados['cliente']['email'] ?></span>
+            </div>
+            <div class="topico d-flex">
+                <p>Dt. Cadastro:</p><span><?= $dados['cliente']['created_at'] ?></span>
+            </div>
         </div>
 
         <div class="botoes">
