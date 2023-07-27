99063d47db5433f3fedc4071f148d67250f972b6
Bruno-RW <bruno_wunsch@hotmail.com>
2023-07-26 21:20:25 -0300

    Atualização da página Minha Conta

--- src/Controller/MinhaContaController.php
+++ src/Controller/MinhaContaController.php
@@ -14,8 +14,31 @@ class MinhaContaController extends FrontController
         $dados = [];
         $dados['topo'] = $this->carregaHTMLTopo();
         $dados['rodape'] = $this->carregaHTMLRodape();
+        
         $dados['cliente'] = $_SESSION['cliente'];
+        $dados['cliente']['email'] = $this->escondeCaracteres($_SESSION['cliente']['email']);
 
+        $dtCadastro = strtotime($dados['cliente']['created_at']);
+        $dados['cliente']['created_at'] = str_replace("-", "/", date("d-m-Y", $dtCadastro));
+        
         Render::front('minha-conta', $dados);
     }
+
+    /**
+     * Método para esconder caractéres de um e-mail
+     *
+     * @param string $email
+     * @return mixed
+    */
+    public function escondeCaracteres (string $email) : string {
+
+            $posArroba = strpos($email, "@");
+    
+            $texto = substr($email, 1, $posArroba-2);
+            $tamanhoTexto = strlen($texto);
+    
+            $email = str_replace($texto, str_repeat("*", $tamanhoTexto), $email);
+
+        return $email;
+      }
 }
--- templates/frontend/pages/minha-conta.php
+++ templates/frontend/pages/minha-conta.php
@@ -4,14 +4,17 @@
 </section>
 
 <section id="info-conta" class="section-p1 d-flex flex-column">
-    <div class="dados">
-        <h2>INFORMAÇÕES PESSOAIS</h2>
-        <div class="conta d-flex justify-content-center">
-            <div class="conteudo d-flex flex-column">
-                <p>Nome:   <span><?= $dados['cliente']['nome'] ?></span></p>
-                <p>E-mail: <span><?= $dados['cliente']['email'] ?></span></p>
-                <p>Senha:  <span><?= $dados['cliente']['senha'] ?></span></p>
-            </div>
+    <h1>Informações Pessoais</h1>
+    <div class="conta d-flex flex-column">
+        <div class="conteudo d-flex flex-column">
+            <p>Nome:   <span><?= $dados['cliente']['nome'] ?></span></p>
+            <p>E-mail: <span><?= $dados['cliente']['email'] ?></span></p>
+            <p>Dt. Cadastro: <span><?= $dados['cliente']['created_at'] ?></span></p>
+        </div>
+
+        <div class="botoes">
+            <a href="#">Alterar e-mail</a>
+            <a href="#">Alterar senha</a>
         </div>
     </div>
 </section>
