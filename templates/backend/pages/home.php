5606b1cb16cdd3398d56c0be7cf30b3e63003f37
Bruno-RW <bruno_wunsch@hotmail.com>
2023-07-28 17:47:44 -0300

    Criação de blocos de dados

--- templates/backend/pages/home.php
+++ templates/backend/pages/home.php
@@ -1,1 +1,21 @@
-<?= retornaHTMLAlertMensagemSessao() ?>
+<div class="conteudo d-flex justify-content-center">
+    <div class="info d-flex flex-column justify-content-center align-items-center">
+        <h3>Qtd. Clientes</h3>
+        <span><?= $dados['info']['clientes'] ?></span>
+    </div>
+
+    <div class="info d-flex flex-column justify-content-center align-items-center">
+        <h3>Qtd. Produtos</h3>
+        <span><?= $dados['info']['produtos'] ?></span>
+    </div>
+
+    <div class="info d-flex flex-column justify-content-center align-items-center">
+        <h3>Qtd. Categorias</h3>
+        <span><?= $dados['info']['categorias'] ?></span>
+    </div>
+
+    <div class="info d-flex flex-column justify-content-center align-items-center">
+        <h3>Qtd. Marcas</h3>
+        <span><?= $dados['info']['marcas'] ?></span>
+    </div>
+</div>
