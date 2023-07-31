99519ae484a79d7c53deab79c7c3d040c84ae914
Bruno-RW <bruno_wunsch@hotmail.com>
2023-07-31 15:37:43 -0300

    Adição de comentário

--- public/assets/js/script-admin.js
+++ public/assets/js/script-admin.js
@@ -1,22 +1,24 @@
-document.querySelectorAll('.table .link-excluir').forEach(link => {
-    // Para cada link com a classe .link-excluir, adiciona-se confirmação
-    link.addEventListener('click', e => {
-        // Impede que o href do link seja acionado
-        e.preventDefault();
+// FUNÇÃO PARA EXCLUIR ELEMENTOS
+    document.querySelectorAll('.table .link-excluir').forEach(link => {
+        // Para cada link com a classe .link-excluir, adiciona-se confirmação
+        link.addEventListener('click', e => {
+            // Impede que o href do link seja acionado
+            e.preventDefault();
 
-        Swal.fire({
-            title: 'ATENÇÃO',
-            html: "Tem certeza que deseja remover este registro?<br><br>Você <b>NÃO</b> poderá reverter esta decisão!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Sim, remover',
-            cancelButtonText: 'Cancelar',
-        }).then((result) => {
-            if (result.isConfirmed) {
-                window.location.href = link.href;
-            }
-        })
+            Swal.fire({
+                title: 'ATENÇÃO',
+                html: "Tem certeza que deseja remover este registro?<br><br>Você <b>NÃO</b> poderá reverter esta decisão!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sim, remover',
+                cancelButtonText: 'Cancelar',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = link.href;
+                }
+            })
+        });
     });
-});
+//
