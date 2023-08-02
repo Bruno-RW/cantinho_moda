127ee2516bc13ad01e9078601578d4ec093e381b
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-08-02 16:48:35 -0300

    Add estilo e conteúdo da pág cancelar-jornal

---
+++ templates/frontend/pages/cancelar-jornal.php
@@ -0,0 +1,10 @@
+<section id="cancelar-jornal" class="section-p1 d-flex align-items-center justify-content-center">
+    <div class="container d-flex flex-column align-items-center justify-content-center">
+        <h1>Cancelar jornal eletrônico?</h1>
+        <p>Para cancelar o recebimento de notificações da loja, basta apertar o botão abaixo</p>
+
+        <div class="btn">
+            <a href="/">Cancelar</a>
+        </div>
+    </div>
+</section>
