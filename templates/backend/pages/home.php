0bb2532ac898454f50d668d912b4fc908791ae84
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-08-03 18:37:56 -0300

    Add responsividade pág Home admin

--- templates/backend/pages/home.php
+++ templates/backend/pages/home.php
@@ -1,20 +1,20 @@
-<div class="conteudo d-flex justify-content-center">
-    <div class="info d-flex flex-column justify-content-center align-items-center">
+<div class="conteudo row d-flex justify-content-center">
+    <div class="info col-4 d-flex flex-column justify-content-center align-items-center">
         <h3>Qtd. Clientes</h3>
         <span><?= $dados['info']['clientes'] ?></span>
     </div>
 
-    <div class="info d-flex flex-column justify-content-center align-items-center">
+    <div class="info col-4 d-flex flex-column justify-content-center align-items-center">
         <h3>Qtd. Produtos</h3>
         <span><?= $dados['info']['produtos'] ?></span>
     </div>
 
-    <div class="info d-flex flex-column justify-content-center align-items-center">
+    <div class="info col-4 d-flex flex-column justify-content-center align-items-center">
         <h3>Qtd. Categorias</h3>
         <span><?= $dados['info']['categorias'] ?></span>
     </div>
 
-    <div class="info d-flex flex-column justify-content-center align-items-center">
+    <div class="info col-4 d-flex flex-column justify-content-center align-items-center">
         <h3>Qtd. Marcas</h3>
         <span><?= $dados['info']['marcas'] ?></span>
     </div>
