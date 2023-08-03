425f77681244dcb1d556e4db48dd7a32f4b1ab72
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-08-03 15:15:20 -0300

    Add coments e removido POST pág cancela-jornal

--- src/Core/App.php
+++ src/Core/App.php
@@ -12,7 +12,7 @@ class App
      * responsável pelo tratamento das rotas
      *
      * @var Router
-     */
+    */
     private static $router;
 
     /**
@@ -21,7 +21,7 @@ class App
      * a partir da URL informada pelo usuário
      *
      * @return void
-     */
+    */
     public static function start()
     {
         // Carrega uma sessão ou inicia uma nova em caso de novo acesso
@@ -44,34 +44,31 @@ class App
      * associadas aos controllers para o FRONTEND
      *
      * @return void
-     */
+    */
     private static function registraRotasDoFrontend()
     {
-        self::$router->get('/','\CantinhoModa\Controller\HomeController@index');
+        // Páginas com GET
+        self::$router->get ('/',                '\CantinhoModa\Controller\HomeController@index');
+        self::$router->get ('/catalogo',        '\CantinhoModa\Controller\CatalogoController@catalogo');
+        self::$router->get ('/sobre',           '\CantinhoModa\Controller\SobreController@sobre');
+        self::$router->get ('/logout',          '\CantinhoModa\Controller\LoginController@logout');
+        self::$router->get ('/favoritos',       '\CantinhoModa\Controller\FavoritosController@favoritos');
+        self::$router->get ('/cancelar-jornal', '\CantinhoModa\Controller\CancelarJornalController@cancelarJornal');
+        
+        // Páginas com GET e POST
+        self::$router->get ('/contato',     '\CantinhoModa\Controller\ContatoController@contato');
+        self::$router->post('/contato',     '\CantinhoModa\Controller\ContatoController@postContato');
 
-        self::$router->get('/catalogo','\CantinhoModa\Controller\CatalogoController@catalogo');
+        self::$router->get ('/login',       '\CantinhoModa\Controller\LoginController@login');
+        self::$router->post('/login',       '\CantinhoModa\Controller\LoginController@postLogin');
 
-        self::$router->get('/sobre','\CantinhoModa\Controller\SobreController@sobre');
+        self::$router->get ('/cadastro',    '\CantinhoModa\Controller\CadastroController@cadastro');
+        self::$router->post('/cadastro',    '\CantinhoModa\Controller\CadastroController@postCadastro');
 
-        self::$router->get('/logout','\CantinhoModa\Controller\LoginController@logout');
+        self::$router->get ('/minha-conta', '\CantinhoModa\Controller\MinhaContaController@minhaConta');
+        self::$router->post('/minha-conta', '\CantinhoModa\Controller\MinhaContaController@postMinhaConta');
 
-        self::$router->get('/favoritos','\CantinhoModa\Controller\FavoritosController@favoritos');
-
-        self::$router->get ('/contato','\CantinhoModa\Controller\ContatoController@contato');
-        self::$router->post('/contato','\CantinhoModa\Controller\ContatoController@postContato');
-
-        self::$router->get ('/login','\CantinhoModa\Controller\LoginController@login');
-        self::$router->post('/login','\CantinhoModa\Controller\LoginController@postLogin');
-
-        self::$router->get ('/cadastro','\CantinhoModa\Controller\CadastroController@cadastro');
-        self::$router->post('/cadastro','\CantinhoModa\Controller\CadastroController@postCadastro');
-
-        self::$router->get ('/minha-conta','\CantinhoModa\Controller\MinhaContaController@minhaConta');
-        self::$router->post('/minha-conta','\CantinhoModa\Controller\MinhaContaController@postMinhaConta');
-
-        self::$router->get ('/cancelar-jornal','\CantinhoModa\Controller\CancelarJornalController@cancelarJornal');
-        self::$router->post('/cancelar-jornal','\CantinhoModa\Controller\CancelarJornalController@postCancelarJornal');
-
+        // Métodos com auxílio AJAX
         self::$router->post('/ajax','\CantinhoModa\Controller\AjaxController@loader');
     }
 
@@ -80,7 +77,7 @@ class App
      * associadas aos controllers para o BACKEND
      *
      * @return void
-     */
+    */
     private static function registraRotasDoBackend()
     {
         self::$router->before('GET|POST', '/admin/.*', function (){
@@ -90,48 +87,51 @@ class App
         });
 
         self::$router->mount('/admin', function() {
-            self::$router->get ('/','\CantinhoModa\Controller\AdminLoginController@login');
-            self::$router->post('/','\CantinhoModa\Controller\AdminLoginController@postLogin');
+            // Página de Login
+            self::$router->get ('/', '\CantinhoModa\Controller\AdminLoginController@login');
+            self::$router->post('/', '\CantinhoModa\Controller\AdminLoginController@postLogin');
 
-            self::$router->get('/home','\CantinhoModa\Controller\AdminHomeController@index');
+            // Página Home e Remover
+            self::$router->get ('/home',                '\CantinhoModa\Controller\AdminHomeController@index');
+            self::$router->get ('/remover/(\w+)/(\d+)', '\CantinhoModa\Controller\AdminRemoveController@acao');
 
-            self::$router->get('/remover/(\w+)/(\d+)','\CantinhoModa\Controller\AdminRemoveController@acao');
+            // Páginas de Admin
+            self::$router->get ('/categorias',         '\CantinhoModa\Controller\AdminCategoriaController@listar');
+            self::$router->get ('/categorias/{valor}', '\CantinhoModa\Controller\AdminCategoriaController@form');
+            self::$router->post('/categorias/{valor}', '\CantinhoModa\Controller\AdminCategoriaController@postForm');
 
-            self::$router->get ('/categorias',        '\CantinhoModa\Controller\AdminCategoriaController@listar');
-            self::$router->get ('/categorias/{valor}','\CantinhoModa\Controller\AdminCategoriaController@form');
-            self::$router->post('/categorias/{valor}','\CantinhoModa\Controller\AdminCategoriaController@postForm');
+            self::$router->get ('/clientes',           '\CantinhoModa\Controller\AdminClienteController@listar');
+            self::$router->get ('/clientes/{valor}',   '\CantinhoModa\Controller\AdminClienteController@form');
+            self::$router->post('/clientes/{valor}',   '\CantinhoModa\Controller\AdminClienteController@postForm');
+  
+            self::$router->get ('/empresas',           '\CantinhoModa\Controller\AdminEmpresaController@listar');
+            self::$router->get ('/empresas/{valor}',   '\CantinhoModa\Controller\AdminEmpresaController@form');
+            self::$router->post('/empresas/{valor}',   '\CantinhoModa\Controller\AdminEmpresaController@postForm');
+  
+            self::$router->get ('/marcas',             '\CantinhoModa\Controller\AdminMarcaController@listar');
+            self::$router->get ('/marcas/{valor}',     '\CantinhoModa\Controller\AdminMarcaController@form');
+            self::$router->post('/marcas/{valor}',     '\CantinhoModa\Controller\AdminMarcaController@postForm');
+  
+            self::$router->get ('/news-msg',           '\CantinhoModa\Controller\AdminNewsMsgController@listar');
+            self::$router->get ('/news-msg/{valor}',   '\CantinhoModa\Controller\AdminNewsMsgController@form');
+            self::$router->post('/news-msg/{valor}',   '\CantinhoModa\Controller\AdminNewsMsgController@postForm');
+  
+            self::$router->get ('/news-usr',           '\CantinhoModa\Controller\AdminNewsUsrController@listar');
+            self::$router->get ('/news-usr/{valor}',   '\CantinhoModa\Controller\AdminNewsUsrController@form');
+            self::$router->post('/news-usr/{valor}',   '\CantinhoModa\Controller\AdminNewsUsrController@postForm');
+  
+            self::$router->get ('/produtos',           '\CantinhoModa\Controller\AdminProdutoController@listar');
+            self::$router->get ('/produtos/{valor}',   '\CantinhoModa\Controller\AdminProdutoController@form');
+            self::$router->post('/produtos/{valor}',   '\CantinhoModa\Controller\AdminProdutoController@postForm');
 
-            self::$router->get ('/clientes',        '\CantinhoModa\Controller\AdminClienteController@listar');
-            self::$router->get ('/clientes/{valor}','\CantinhoModa\Controller\AdminClienteController@form');
-            self::$router->post('/clientes/{valor}','\CantinhoModa\Controller\AdminClienteController@postForm');
+            self::$router->get ('/usuarios',           '\CantinhoModa\Controller\AdminUsuarioController@listar');
+            self::$router->get ('/usuarios/{valor}',   '\CantinhoModa\Controller\AdminUsuarioController@form');
+            self::$router->post('/usuarios/{valor}',   '\CantinhoModa\Controller\AdminUsuarioController@postForm');
 
-            self::$router->get ('/empresas',        '\CantinhoModa\Controller\AdminEmpresaController@listar');
-            self::$router->get ('/empresas/{valor}','\CantinhoModa\Controller\AdminEmpresaController@form');
-            self::$router->post('/empresas/{valor}','\CantinhoModa\Controller\AdminEmpresaController@postForm');
-
-            self::$router->get ('/marcas',        '\CantinhoModa\Controller\AdminMarcaController@listar');
-            self::$router->get ('/marcas/{valor}','\CantinhoModa\Controller\AdminMarcaController@form');
-            self::$router->post('/marcas/{valor}','\CantinhoModa\Controller\AdminMarcaController@postForm');
-
-            self::$router->get ('/news-msg',        '\CantinhoModa\Controller\AdminNewsMsgController@listar');
-            self::$router->get ('/news-msg/{valor}','\CantinhoModa\Controller\AdminNewsMsgController@form');
-            self::$router->post('/news-msg/{valor}','\CantinhoModa\Controller\AdminNewsMsgController@postForm');
-
-            self::$router->get ('/news-usr',        '\CantinhoModa\Controller\AdminNewsUsrController@listar');
-            self::$router->get ('/news-usr/{valor}','\CantinhoModa\Controller\AdminNewsUsrController@form');
-            self::$router->post('/news-usr/{valor}','\CantinhoModa\Controller\AdminNewsUsrController@postForm');
-
-            self::$router->get ('/produtos',        '\CantinhoModa\Controller\AdminProdutoController@listar');
-            self::$router->get ('/produtos/{valor}','\CantinhoModa\Controller\AdminProdutoController@form');
-            self::$router->post('/produtos/{valor}','\CantinhoModa\Controller\AdminProdutoController@postForm');
-
-            self::$router->get ('/usuarios',        '\CantinhoModa\Controller\AdminUsuarioController@listar');
-            self::$router->get ('/usuarios/{valor}','\CantinhoModa\Controller\AdminUsuarioController@form');
-            self::$router->post('/usuarios/{valor}','\CantinhoModa\Controller\AdminUsuarioController@postForm');
-
-            self::$router->get ('/imagens/(\w+)/(\d+)',      '\CantinhoModa\Controller\AdminImagemController@listar');
-            self::$router->get ('/imagens/(\w+)/(\d+)/(\w+)','\CantinhoModa\Controller\AdminImagemController@form');
-            self::$router->post('/imagens/(\w+)/(\d+)/(\w+)','\CantinhoModa\Controller\AdminImagemController@postForm');
+            // Páginas de Imagens
+            self::$router->get ('/imagens/(\w+)/(\d+)',       '\CantinhoModa\Controller\AdminImagemController@listar');
+            self::$router->get ('/imagens/(\w+)/(\d+)/(\w+)', '\CantinhoModa\Controller\AdminImagemController@form');
+            self::$router->post('/imagens/(\w+)/(\d+)/(\w+)', '\CantinhoModa\Controller\AdminImagemController@postForm');
         });
     }
 
@@ -139,7 +139,7 @@ class App
      * Registra rota genérica para erro de URL digitada
      *
      * @return void
-     */
+    */
     private static function registra404Generico()
     {
         self::$router->set404(function() {
@@ -154,7 +154,7 @@ class App
      * carrega o ID da sessão gravada no dispositivo do usuário
      *
      * @return void
-     */
+    */
     private static function carregaSessao()
     {
         session_start();
