b6db63bc9e9cb7db569d452f4af57ec8fa6f4da3
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-08-03 14:39:29 -0300

    Alterado estilo de pág cancelar-jornal

--- templates/frontend/pages/cancelar-jornal.php
+++ templates/frontend/pages/cancelar-jornal.php
@@ -1,10 +1,16 @@
 <section id="cancelar-jornal" class="section-p1 d-flex align-items-center justify-content-center">
     <div class="container d-flex flex-column align-items-center justify-content-center">
         <h1>Cancelar jornal eletrônico?</h1>
-        <p>Para cancelar o recebimento de notificações da loja, basta apertar o botão abaixo</p>
+        <p>Para cancelar o recebimento de notificações da loja, basta informar o e-mail desejado e apertar o botão abaixo</p>
+
+        <div class="form d-flex justify-content-center align-items-center">
+            <div class="input-box">
+                <i class="fa-solid fa-envelope"></i>
+                <input type="text" id="cancelaNews" name="email" placeholder="E-mail" required>
+            </div>
 
         <div class="btn">
-            <a href="/">Cancelar</a>
+            <a href="#">Cancelar</a>
         </div>
     </div>
 </section>
